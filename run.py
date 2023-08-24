753096fe28c6eda96c755769e5e0df8f5b962ed7
Samuelsilvestre <fnndc90@gmail.com>
2023-08-24 15:48:53 -0300

    'generic'

--- run.py
+++ run.py
@@ -6,6 +6,23 @@ from src.schema.usuario import User
 class Ex(Db):
     url = 'mysql+mysqldb://root:password@localhost/db'
 
+    def objects(self):
+        session =  super().objects()
+        queryset = session.query(User).all()
+        return self.select(queryset)
+
+    def select(self, queryset):
+        data = [ {
+            'id': attr.id,
+            'email': attr.email,
+            'user_name': attr.user_name
+        } for attr in queryset ]
+        return data
+
+
+
+        
+
 
 
 
